d321a46ce6ca286f4b3a99567ffa7d278bff9b32
Devon Burnham <novedevo@gmail.com>
2020-02-25 13:22:01 -0800

    code for lab5d

--- lab5d_Burnham_Devon.m
+++ lab5d_Burnham_Devon.m
@@ -5,6 +5,7 @@
 % Instructor: Dr. Herbert H. Tsang
 % Description: Solves the linear equation system Ax=b with Gauss-Jordan
 % elimination
+% 
 % Due date: 2020/02/29
 %
 % Author: Devon Sawatsky Burnham
@@ -16,3 +17,25 @@
 %
 % Sign here: Devon Sawatsky Burnham
 %############################################################################
+
+A = [4 3 1;3 7 -1; 1 -1 9];
+C = [1 -1; 4 7; 9 5];
+
+solveLinSystem(A,C)
+
+function solveLinSystem(A,b)
+    
+    % Find the row-reduced echelon form of A
+    R = rref(A);
+
+    if rank(A) < size(A)
+        disp("Zero rows exist, thus no unique solution exists. Quitting...")
+    else
+        disp("No zero rows exist, thus a unique solution exists")
+        x = A\b;
+        disp('Unique solution: {x} =')
+        disp(x);
+    end
+end
+
+
